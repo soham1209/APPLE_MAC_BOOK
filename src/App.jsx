e302655de39c682bc1323fe88343e97a944f5f9d
soham1209 <sohamns1234@gmail.com>
2025-10-20 00:35:10 +0530

    feat: Add Features component with 3D model interaction and video synchronization

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import ProductViewer from "./components/ProductViewer"
 import Showcase from "./components/Showcase"
 import Performance from "./components/Performance"
+import Features from "./components/Features"
 
 import gsap from "gsap"
 import {ScrollTrigger,SplitText} from "gsap/all"
@@ -17,6 +18,7 @@ function App() {
       <ProductViewer/>
       <Showcase/>
       <Performance/>
+      <Features/>
     </main>
   )
 }
